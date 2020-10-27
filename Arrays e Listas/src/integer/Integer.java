d04d84b78f4fb27a7d8d72990e871cbbe71d6c73
Vinícius Belloti <marcusvbelloti@gmail.com>
2020-10-27 13:34:49 -0300

    Projeto Criado

---
+++ Arrays e Listas/src/integer/Integer.java
@@ -0,0 +1,20 @@
+package integer;
+
+public class Integer {
+
+	public static void main(String[] args) {
+		//TIPO INTEGER NAO FUNCIONA, MAS APARENTEMENTE NÃO HÁ 
+		//NECESSIDADE DE SE USAR POIS O PRÓPRIO INT JÁ REALIZA FUNÇÃO DE WRAPPER
+		int x = 20;
+		
+		int obj = x;
+		
+		System.out.println(obj);
+		
+		int y = obj;
+		
+		System.out.println(y);
+		
+	}
+
+}
---
+++ Exercicio Fixacao/src/application/Main.java
@@ -0,0 +1,54 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Account;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		//variável declarada para instanciação após o if, do contrário estaria limitada a condição
+		Account account;
+		
+		System.out.println("Digite o número da conta: ");
+		int number = sc.nextInt();
+		System.out.print("Digite o nome do proprietário da conta: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Será efetuado deposito inicial? (s/n) ");
+		char response = sc.next().charAt(0);
+			if (response == 's') {
+				System.out.print("Digite o valor do deposito inicial: ");
+				double initialDeposit = sc.nextDouble();
+				account = new Account(number, holder, initialDeposit);
+			} else {
+				account = new Account(number, holder);
+			}
+		
+		System.out.println();
+		System.out.println("Dados da conta: ");
+		System.out.println(account);
+		
+		System.out.println();
+		System.out.print("Digite o valor do depósito: ");
+		double depositValue = sc.nextDouble();
+		account.deposit (depositValue);
+		System.out.println("Dados atualizados da conta: ");
+		System.out.print(account);
+		
+		System.out.println();
+		System.out.print("Digite o valor a ser sacado: ");
+		double withdrawValue = sc.nextDouble();
+		account.withdraw(withdrawValue);
+		System.out.println("Dados atualizados da conta: ");
+		System.out.print(account);
+		
+		
+		sc.close();
+	}
+
+}
