39d5f4f5f14ba44c0c0dd02d4da142eeb34ae143
Vinícius Belloti <marcusvbelloti@gmail.com>
2020-10-29 13:31:50 -0300

    inicial

---
+++ Enum e Comp Ex2/src/entities/Post.java
@@ -0,0 +1,98 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Post {
+	
+	private static 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	
+	private Date moment;
+	private String title;
+	private String content;
+	private Integer likes;
+	
+	private List<Comment> comments = new ArrayList<>();
+	
+	public Post() {
+	}
+
+	public Post(Date moment, String title, String content, Integer likes) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
+
+	//não podemos ter uma lista substituindo uma lista, portanto é excluído o método getComment e instanciaremos um addComent
+	//adicionar e remover comentários
+	//tipo MUITOS
+	
+	public void addComment(Comment comment) {
+		comments.add(comment);
+	}
+	
+	public void removeComment(Comment comment) {
+		comments.remove(comment);
+	}
+	
+	//utilização de string builder para não ser necessário criar espaço em memória para cada comentário com textos gigantes,
+	//instanciando a string para um formato padronizado
+	//CONSTRUTOR DE STRING
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(title + "\n");
+		sb.append(likes);
+		sb.append(" Likes - ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append(content + "\n");
+		sb.append("Comments: \n");
+		for (Comment c : comments) {
+			sb.append(c.getText());
+		}
+		return sb.toString();
+	}
+	
+	
+	
+	
+	
+}
---
+++ Enum e Comp Exercícios/src/entities/HourContract.java
@@ -0,0 +1,49 @@
+package entities;
+
+import java.util.Date;
+
+public class HourContract {
+	
+	private Date date;
+	private double valuePerHour;
+	private Integer hours;
+	
+	//Construtor Padrão sem instâncias
+	public HourContract() {	
+	}
+
+	public HourContract(Date date, Double valuePerHour, Integer hours) {
+		this.date = date;
+		this.valuePerHour = valuePerHour;
+		this.hours = hours;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Integer getHours() {
+		return hours;
+	}
+
+	public void setHours(Integer hours) {
+		this.hours = hours;
+	}
+
+	public Double getValuePerHour() {
+		return valuePerHour;
+	}
+
+	public void setValuePerHour(double valuePerHour) {
+		this.valuePerHour = valuePerHour;
+	}
+	
+	public double totalValue() {
+		return valuePerHour * hours;
+	}
+
+}
